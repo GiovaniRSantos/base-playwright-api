b0458ff8ef1e79b353c5986d65b73eaa013cba59
Giovani S Rodrigues <giovani.srodrigues@hotmail.com>
2024-09-07 16:28:20 -0300

    definicao model produto

--- tests/products.spec.ts
+++ tests/products.spec.ts
@@ -3,6 +3,18 @@ import { getResource } from '../src/helpers'
 
 test('get all products', async () => {
   const products = await getResource('products')
-  console.log('GET products:', products)
   expect(products).toBeDefined()
+  expect(Array.isArray(products)).toBe(true)
+
+  products.forEach((item: Product) => {
+    expect(item).toHaveProperty('id')
+    expect(item).toHaveProperty('title')
+    expect(item).toHaveProperty('price')
+    expect(item).toHaveProperty('description')
+    expect(item).toHaveProperty('category')
+    expect(item).toHaveProperty('image')
+    expect(item).toHaveProperty('rating')
+    expect(item.rating).toHaveProperty('rate')
+    expect(item.rating).toHaveProperty('count')
+  })
 })
